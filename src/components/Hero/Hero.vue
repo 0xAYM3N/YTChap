745ccde8dd8d03093f2d2a5dd1ead8e1fbdabee3
0xAYM3N <aymen.dev10@gmail.com>
2025-08-08 10:14:52 +0100

    intial commit

---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,11 @@
+<script setup>
+import "./Hero.css"
+</script>
+
+<template>
+  <div class="hero">
+    <div class="container">
+      <h1>السلام عليكم</h1>
+    </div>
+  </div>
+</template>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import './Navbar.css'
+</script>
+
+<template>
+  <div class="navbar">
+    <div class="container">
+      <div class="logo">
+        <a href="/">Athir</a>
+      </div>
+
+      <div class="links">
+        <a href="/Home">Home</a>
+        <a href="/Home">Home</a>
+        <a href="/Home">Home</a>
+        <a href="/Home">Home</a>
+      </div>
+    </div>
+  </div>
+</template>
