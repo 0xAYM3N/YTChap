688bf0cbfbe0df9399ef610c81c71275babe441f
0xAYM3N <aymen.dev10@gmail.com>
2025-08-11 13:21:44 +0100

    wip

--- src/components/YTChap/YTChap.vue
+++ src/components/YTChap/YTChap.vue
@@ -7,20 +7,20 @@ import './YTChap.css'
     <div class="container">
       <div class="video">
         <label for="images" class="select-file">
-          Click To Select
+          Click To Select Video
           <input id="images" type="file" accept="image/*" />
         </label>
         <div class="control-btns">
           <button type="button">Start</button>
-          <button type="button">Pouse</button>
-          <button type="button">Break Point</button>
+          <button type="button"><i class="fa-solid fa-play"></i></button>
+          <button type="button"><i class="fa-solid fa-pause"></i></button>
         </div>
       </div>
       <div class="chapter-container">
-        <h1>Chapter</h1>
-        <div class="video-chapters">
+        <h1>Chapters</h1>
+        <div class="chapters-panel">
           <h3>00:00 - 10:00</h3> 
-          <h3>00:00 - 10:00</h3> 
+          <h3>10:00 - 20:00</h3> 
         </div>
       </div>
     </div>
